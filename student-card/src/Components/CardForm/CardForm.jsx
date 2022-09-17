bdda4e83fcbfb79bd2d64b383f64cb62984ebe08
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-09-18 02:33:25 +0300

    added styles for the form and installed the React Hook Form library

--- student-card/src/Components/CardForm/CardForm.jsx
+++ student-card/src/Components/CardForm/CardForm.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import style from "./CardForm.module.scss";
+import { useForm } from "react-hook-form";
 
 const CardForm = () => {
-  return <div>card form</div>;
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // fromState: { errors },
+  } = useForm({ mode: "onSubmit" });
+
+  const onSubmit = (data) => console.log(data);
+  // console.log("register: ", register);
+  // console.log("watch: ", watch);
+  // console.log("handleSubmit: ", handleSubmit);
+  return (
+    <div className={style.form_container}>
+      <h4 className={style.form_container_h}>
+        Пожалуйста, введите свои данные
+      </h4>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={style.form_container_form}
+      >
+        <label>Имя</label>
+        <input
+          {...register("name")}
+          className={style.form_container_form_field}
+        />
+        <label>Фамилия</label>
+        <input
+          {...register("surname")}
+          className={style.form_container_form_field}
+        />
+        <label>Год рождения</label>
+        <input
+          {...register("year")}
+          className={style.form_container_form_field}
+        />
+        <label>Портфолио</label>
+        <input
+          {...register("portfolio")}
+          className={style.form_container_form_field}
+        />
+        {/* {errors.exampleRequired && <span>This field is required</span>} */}
+        <input type="submit" className={style.form_container_form_button} />
+      </form>
+    </div>
+  );
 };
 
 export default CardForm;
